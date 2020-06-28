9f12c778384fcf369f754ff4231e3d9c5b684772
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-06-28 19:41:26 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,8 @@ var a = new Date();
 var b = new Date();
 var c = new Date();
 
-    setTimeout(function () {
-        fetch(`https://corona.lmao.ninja/v2/countries/br`)
+console.log('Buscando informações sobre o COVID...')
+fetch(`https://corona.lmao.ninja/v2/countries/br`)
     .then(res => res.json())
     .then(data1 => {
       let casos = data1.cases.toLocaleString();
@@ -31,7 +31,7 @@ var c = new Date();
       let testesM = data1.testsPerOneMillion.toLocaleString();
       let ativos = data1.active.toLocaleString();
 
-covid_brasil = (`Confirmados: (Total: ${casos} | Hoje: +${casosHoje})
+    covid_brasil = (`Confirmados: (Total: ${casos} | Hoje: +${casosHoje})
 Mortes: (Total: ${mortes} | Hoje: +${mortesHoje})
 Recuperados: ${recuperados}
 Em Risco: ${emRisco}
@@ -45,17 +45,58 @@ Atualizados: ${c.getDate()}.0${b.getMonth()+1}.${a.getFullYear()}
 
 #covid #covid19 #brasil #corona #coronavirus`)
 
-T.post(
-    'statuses/update',
-    { status: covid_brasil },
-    (err, data, response) => {
-      console.log(err, data, response);
-    }
-  )
-  
-console.log(covid_brasil)
+      T.post(
+        'statuses/update',
+        { status: covid_brasil },
+        (err, data, response) => {
+        console.log(err, data, response);
+        }
+      )
+    });
+console.log('Criando um tweet com as informações obtidas...')
+
+client.on('ready', () => {
+  setInterval(() => {
+    console.log('Buscando informações sobre o COVID...')
+    fetch(`https://corona.lmao.ninja/v2/countries/br`)
+    .then(res => res.json())
+    .then(data1 => {
+      let casos = data1.cases.toLocaleString();
+      let casosHoje = data1.todayCases.toLocaleString();
+      let mortes = data1.deaths.toLocaleString();
+      let mortesHoje = data1.todayDeaths.toLocaleString();
+      let recuperados = data1.recovered.toLocaleString();
+      let emRisco = data1.critical.toLocaleString();
+      let mortesM = data1.deathsPerOneMillion.toLocaleString();
+      let casosM = data1.casesPerOneMillion.toLocaleString();
+      let testesM = data1.testsPerOneMillion.toLocaleString();
+      let ativos = data1.active.toLocaleString();
+
+    covid_brasil = (`Confirmados: (Total: ${casos} | Hoje: +${casosHoje})
+Mortes: (Total: ${mortes} | Hoje: +${mortesHoje})
+Recuperados: ${recuperados}
+Em Risco: ${emRisco}
+Ativos: ${ativos}
+
+Testes/1Mi: ${testesM}
+Casos/1Mi: ${casosM}
+Mortes/1Mi: ${mortesM}
+
+Atualizados: ${c.getDate()}.0${b.getMonth()+1}.${a.getFullYear()}
+
+#covid #covid19 #brasil #corona #coronavirus`)
+
+      T.post(
+        'statuses/update',
+        { status: covid_brasil },
+        (err, data, response) => {
+        console.log(err, data, response);
+        }
+      )
+      console.log('Criando um tweet com as informações obtidas...')
+    });
+  }, 3600000);
 });
-}, 7200000);
 
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
