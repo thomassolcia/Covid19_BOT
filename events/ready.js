aee2fcd061c23a1140c759713f4a70f7e4762436
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-06-29 05:29:17 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
 module.exports = async client => {
     console.log('\n     [STATUS]')
     console.log('\nO BOT JÁ ESTÁ OPERANDO!');
-    client.user.setActivity("Twitter | @Brazil_Covid19", { type: "PLAYING" });
-
+    client.user.setActivity("@COVID_Brazil", { type: "PLAYING" });
 }
